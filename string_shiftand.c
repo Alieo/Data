72d4c73fc56d9672f8ebb279d0258d2e100af843
Alieo <m18249236296@163.com>
2019-03-06 10:44:36 +0800

    shift-and算法

--- string_shiftand.c
+++ string_shiftand.c
@@ -5,26 +5,32 @@
 	> Created Time: Fri 04 Jan 2019 11:01:18 AM CST
  ************************************************************************/
 //字符串匹配算法之shift-and算法
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
-int shift(char *str, char *pat) {
-    int code[120] = {0}, len = 0;
-    for (len = 0; pat[len]; len++) {
+#include <stdlib.h>
+
+#define TEST(func, a, b) { \
+    printf("%s(\"%s\", \"%s\") = %d\n", #func, a, b, func(a, b)); \
+}
+int ShiftAnd(char *str, char *pattern) {
+    #define BASE 128
+    int code[BASE] = {0}, len = 0;
+    for (len = 0; pattern[len]; len++) {
         //将模式串各个字符出现的位置转为二进制。。
-        code[pat[len]] |= (1 << len);
+        code[pattern[len]] |= (1 << len);
     }
     int p = 0;
     for (int i = 0; str[i]; i++) {
-        p = (p << 1) | 1 & code[str[i]];
-        printf("%d\n", p);
+        p = (p << 1 | 1) & code[str[i]];
         if (p & (1 << (len - 1))) return i - len + 1;
     }
     return -1;
+    #undef BASE
 }
-
 int main() {
-    char str[100] = "aebacbaer";
-    char pat[100] = "acb";
-    shift(str, pat);
+    char str[100], pattern[30];
+    while (scanf("%s%s", str, pattern) != EOF) {
+        TEST(ShiftAnd, str, pattern);
+    }
     return 0;
 }
